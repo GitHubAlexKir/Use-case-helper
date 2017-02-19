52d7a5fa86d6d228b283726fd4166448f42b82da
Alex <Alex@MSI>
2017-02-19 15:49:06 +0100

    Started on UseCases

---
+++ UseCaseHelper/UseCaseHelper/Beschrijving.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace UseCaseHelper
+{
+    partial class Beschrijving
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Naamtextbox = new System.Windows.Forms.TextBox();
+            this.Samenvattingtextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Actortextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Aannamentextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Beschrijvingtextbox = new System.Windows.Forms.RichTextBox();
+            this.uitzonderingentextbox = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Resultaattextbox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Invoerbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Naam";
+            // 
+            // Naamtextbox
+            // 
+            this.Naamtextbox.Location = new System.Drawing.Point(112, 6);
+            this.Naamtextbox.Name = "Naamtextbox";
+            this.Naamtextbox.Size = new System.Drawing.Size(462, 22);
+            this.Naamtextbox.TabIndex = 1;
+            // 
+            // Samenvattingtextbox
+            // 
+            this.Samenvattingtextbox.Location = new System.Drawing.Point(112, 33);
+            this.Samenvattingtextbox.Name = "Samenvattingtextbox";
+            this.Samenvattingtextbox.Size = new System.Drawing.Size(462, 22);
+            this.Samenvattingtextbox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Samenvatting";
+            // 
+            // Actortextbox
+            // 
+            this.Actortextbox.Location = new System.Drawing.Point(112, 62);
+            this.Actortextbox.Name = "Actortextbox";
+            this.Actortextbox.Size = new System.Drawing.Size(462, 22);
+            this.Actortextbox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Actors";
+            // 
+            // Aannamentextbox
+            // 
+            this.Aannamentextbox.Location = new System.Drawing.Point(112, 90);
+            this.Aannamentextbox.Name = "Aannamentextbox";
+            this.Aannamentextbox.Size = new System.Drawing.Size(462, 22);
+            this.Aannamentextbox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Aannamen";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Beschrijving";
+            // 
+            // Beschrijvingtextbox
+            // 
+            this.Beschrijvingtextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Beschrijvingtextbox.Location = new System.Drawing.Point(112, 120);
+            this.Beschrijvingtextbox.Name = "Beschrijvingtextbox";
+            this.Beschrijvingtextbox.Size = new System.Drawing.Size(462, 125);
+            this.Beschrijvingtextbox.TabIndex = 9;
+            this.Beschrijvingtextbox.Text = "";
+            // 
+            // uitzonderingentextbox
+            // 
+            this.uitzonderingentextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uitzonderingentextbox.Location = new System.Drawing.Point(112, 249);
+            this.uitzonderingentextbox.Name = "uitzonderingentextbox";
+            this.uitzonderingentextbox.Size = new System.Drawing.Size(462, 125);
+            this.uitzonderingentextbox.TabIndex = 11;
+            this.uitzonderingentextbox.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(2, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Uitzonderingen";
+            // 
+            // Resultaattextbox
+            // 
+            this.Resultaattextbox.Location = new System.Drawing.Point(112, 382);
+            this.Resultaattextbox.Name = "Resultaattextbox";
+            this.Resultaattextbox.Size = new System.Drawing.Size(462, 22);
+            this.Resultaattextbox.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 385);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Resultaat";
+            // 
+            // Invoerbtn
+            // 
+            this.Invoerbtn.Location = new System.Drawing.Point(499, 419);
+            this.Invoerbtn.Name = "Invoerbtn";
+            this.Invoerbtn.Size = new System.Drawing.Size(75, 23);
+            this.Invoerbtn.TabIndex = 14;
+            this.Invoerbtn.Text = "Invoeren";
+            this.Invoerbtn.UseVisualStyleBackColor = true;
+            // 
+            // Beschrijving
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 464);
+            this.Controls.Add(this.Invoerbtn);
+            this.Controls.Add(this.Resultaattextbox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.uitzonderingentextbox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Beschrijvingtextbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Aannamentextbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Actortextbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Samenvattingtextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Naamtextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Beschrijving";
+            this.Text = "Beschrijving";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Naamtextbox;
+        private System.Windows.Forms.TextBox Samenvattingtextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Actortextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Aannamentextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox Beschrijvingtextbox;
+        private System.Windows.Forms.RichTextBox uitzonderingentextbox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Resultaattextbox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button Invoerbtn;
+    }
+}
--- UseCaseHelper/UseCaseHelper/Usecase.cs
+++ UseCaseHelper/UseCaseHelper/Usecase.cs
@@ -8,5 +8,21 @@ namespace UseCaseHelper
 {
     class Usecase
     {
+        public string naam;
+        public string samenvatting;
+        public int[] actoren;
+        public string aannamen;
+        public string beschrijving;
+        public string resultaat;
+
+        public Usecase(string n,string s, int[]a,string aa,string b, string r)
+        {
+            naam = n;
+            samenvatting = s;
+            actoren = a;
+            aannamen = aa;
+            beschrijving = b;
+            resultaat = r;
+        }
     }
 }
